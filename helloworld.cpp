4b84ded41788251b2eabe5d25a4a5b34a97d0d79
Xinjian Niu <niuxinjianhit@163.com>
2023-11-29 16:38:54 -0800

    222

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,5 +4,6 @@ int main()
 {
   std::cout << "Hello World!" << std::endl;
   std::cout << "add code and push the mitified code!" << std::endl;
+  std::cout << "Hello World2!" << std::endl;
   return 0;
 }
